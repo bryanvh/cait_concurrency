5519516a9347dd1829122a4fe6830d0be9db8581
Bryan Van Hook <bryanvh@yahoo.com>
2014-05-11 21:00:31 -0500

    Refactored most classes to be immutable

---
+++ src/main/java/com/github/bryanvh/concurrency/EffectGroup.java
@@ -0,0 +1,51 @@
+package com.github.bryanvh.concurrency;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+@XmlRootElement()
+public class EffectGroup {
+	private List<Effect> effects = new ArrayList<Effect>();
+
+	public List<Effect> getEffects() {
+		return effects;
+	}
+
+	@XmlTransient
+	public void setMelee(int i) {
+		effects.add(Effect.createMelee(i));
+	}
+
+	@XmlTransient
+	public void setMagic(int i) {
+		effects.add(Effect.createMagic(i));
+	}
+
+	@XmlTransient
+	public void setArmor(int i) {
+		effects.add(Effect.createArmor(i));
+	}
+
+	@XmlTransient
+	public void setWard(int i) {
+		effects.add(Effect.createWard(i));
+	}
+
+	@XmlTransient
+	public void setStun(int i) {
+		effects.add(Effect.createStun(i));
+	}
+
+	@XmlTransient
+	public void setBane(Condition c) {
+		effects.add(Effect.createBane(c));
+	}
+
+	@XmlTransient
+	public void setAura(Condition c) {
+		effects.add(Effect.createAura(c));
+	}
+}
